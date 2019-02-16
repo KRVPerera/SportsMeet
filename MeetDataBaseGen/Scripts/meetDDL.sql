ee07b6993fc2d9b5430163ebd354236d499a8c77
KRVPerera <rukshan.viduranga@gmail.com>
2019-02-16 16:34:49 +0530

    schema update of DB added events , playerEvents

--- MeetDataBaseGen/Scripts/meetDDL.sql
+++ MeetDataBaseGen/Scripts/meetDDL.sql
@@ -3,6 +3,15 @@
     name     TEXT (100) 
 );
 
+CREATE TABLE Events (
+    id       INT     PRIMARY KEY,
+    name     TEXT    UNIQUE
+                     NOT NULL,
+    gender   BOOLEAN,
+    agelimit INTEGER NOT NULL
+);
+
+
 CREATE TABLE Players (
     [no]      INTEGER      PRIMARY KEY AUTOINCREMENT,
     number    TEXT         UNIQUE,
@@ -15,3 +24,26 @@ CREATE TABLE Players (
 
     REFERENCES Schools (schoolId) ON DELETE SET NULL ON UPDATE CASCADE
 );
+
+CREATE TABLE PlayerEvents (
+    eventId  INT,
+    playerId INT,
+
+    FOREIGN KEY (
+        playerId
+    )
+
+    REFERENCES Players (number) ON DELETE CASCADE
+                                ON UPDATE CASCADE,
+    PRIMARY KEY (
+        eventId,
+        playerId
+    ),
+    FOREIGN KEY (
+        eventId
+    )
+    REFERENCES Events (id) ON DELETE CASCADE
+                           ON UPDATE CASCADE
+);
+
+
