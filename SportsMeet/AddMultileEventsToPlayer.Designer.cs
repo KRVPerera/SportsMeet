0e42665eebaab66f86add7554d9bb0837ec71d37
KRVPerera <rukshan.viduranga@gmail.com>
2019-03-02 22:13:52 +0530

    add event tab player information

--- SportsMeet/AddMultileEventsToPlayer.Designer.cs
+++ SportsMeet/AddMultileEventsToPlayer.Designer.cs
@@ -29,20 +29,122 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddMultipleEventsToPlayer));
+            this.lblPlayerNumber = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelAddEventsPlayerNumber = new System.Windows.Forms.Label();
+            this.labelAddEventsPlayerFullName = new System.Windows.Forms.Label();
+            this.labelAddEventsPlayerGender = new System.Windows.Forms.Label();
+            this.labelAddEventsPlayerAge = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblPlayerNumber
+            // 
+            this.lblPlayerNumber.AutoSize = true;
+            this.lblPlayerNumber.Location = new System.Drawing.Point(13, 9);
+            this.lblPlayerNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPlayerNumber.Name = "lblPlayerNumber";
+            this.lblPlayerNumber.Size = new System.Drawing.Size(76, 13);
+            this.lblPlayerNumber.TabIndex = 3;
+            this.lblPlayerNumber.Text = "Player Number";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Full Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 35);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Gender";
+            // 
+            // labelAddEventsPlayerNumber
+            // 
+            this.labelAddEventsPlayerNumber.AutoSize = true;
+            this.labelAddEventsPlayerNumber.Location = new System.Drawing.Point(97, 8);
+            this.labelAddEventsPlayerNumber.Name = "labelAddEventsPlayerNumber";
+            this.labelAddEventsPlayerNumber.Size = new System.Drawing.Size(35, 13);
+            this.labelAddEventsPlayerNumber.TabIndex = 7;
+            this.labelAddEventsPlayerNumber.Text = "label4";
+            // 
+            // labelAddEventsPlayerFullName
+            // 
+            this.labelAddEventsPlayerFullName.AutoSize = true;
+            this.labelAddEventsPlayerFullName.Location = new System.Drawing.Point(97, 21);
+            this.labelAddEventsPlayerFullName.Name = "labelAddEventsPlayerFullName";
+            this.labelAddEventsPlayerFullName.Size = new System.Drawing.Size(35, 13);
+            this.labelAddEventsPlayerFullName.TabIndex = 8;
+            this.labelAddEventsPlayerFullName.Text = "label5";
+            // 
+            // labelAddEventsPlayerGender
+            // 
+            this.labelAddEventsPlayerGender.AutoSize = true;
+            this.labelAddEventsPlayerGender.Location = new System.Drawing.Point(97, 35);
+            this.labelAddEventsPlayerGender.Name = "labelAddEventsPlayerGender";
+            this.labelAddEventsPlayerGender.Size = new System.Drawing.Size(35, 13);
+            this.labelAddEventsPlayerGender.TabIndex = 9;
+            this.labelAddEventsPlayerGender.Text = "label6";
+            // 
+            // labelAddEventsPlayerAge
+            // 
+            this.labelAddEventsPlayerAge.AutoSize = true;
+            this.labelAddEventsPlayerAge.Location = new System.Drawing.Point(97, 48);
+            this.labelAddEventsPlayerAge.Name = "labelAddEventsPlayerAge";
+            this.labelAddEventsPlayerAge.Size = new System.Drawing.Size(35, 13);
+            this.labelAddEventsPlayerAge.TabIndex = 10;
+            this.labelAddEventsPlayerAge.Text = "label7";
             // 
             // AddMultipleEventsToPlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAddEventsPlayerAge);
+            this.Controls.Add(this.labelAddEventsPlayerGender);
+            this.Controls.Add(this.labelAddEventsPlayerFullName);
+            this.Controls.Add(this.labelAddEventsPlayerNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlayerNumber);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AddMultipleEventsToPlayer";
             this.Text = "Add Player to Events";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPlayerNumber;
+        internal System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelAddEventsPlayerNumber;
+        private System.Windows.Forms.Label labelAddEventsPlayerFullName;
+        private System.Windows.Forms.Label labelAddEventsPlayerGender;
+        private System.Windows.Forms.Label labelAddEventsPlayerAge;
     }
 }
