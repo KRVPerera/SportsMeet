3caeb9bdd56b27fd6c80c4dbba0e89d76e96efa7
isururathnayaka <isururathnayaka@gmail.com>
2019-07-16 18:21:06 +0530

    distict -> province, player tab UI changes

--- MeetDataBaseGen/Scripts/meetDML.sql
+++ MeetDataBaseGen/Scripts/meetDML.sql
@@ -8,35 +8,45 @@ CREATE TABLE IF NOT EXISTS EducationZones (
 	name  TEXT      UNIQUE
 );
 
-INSERT INTO Districts (name) VALUES  ('Galle');
-INSERT INTO Districts (name) VALUES  ('Hambantota');
-INSERT INTO Districts (name) VALUES  ('Matara');
+INSERT INTO Districts (name) VALUES  ('Central Province');
+INSERT INTO Districts (name) VALUES  ('Eastern Province');
+INSERT INTO Districts (name) VALUES  ('Northern Province');
+INSERT INTO Districts (name) VALUES  ('North Central Province');
+INSERT INTO Districts (name) VALUES  ('North Western Province');
+INSERT INTO Districts (name) VALUES  ('Sabaragamuwa Province');
+INSERT INTO Districts (name) VALUES  ('Southern Province');
+INSERT INTO Districts (name) VALUES  ('Uva Province');
+INSERT INTO Districts (name) VALUES  ('Western Province');
 
-INSERT INTO Districts (name) VALUES  ('Ampara');
-INSERT INTO Districts (name) VALUES  ('Anuradhapura');
-INSERT INTO Districts (name) VALUES  ('Badulla');
-INSERT INTO Districts (name) VALUES  ('Batticaloa');
-INSERT INTO Districts (name) VALUES  ('Colombo');
+--INSERT INTO Districts (name) VALUES  ('Galle');
+--INSERT INTO Districts (name) VALUES  ('Hambantota');
+--INSERT INTO Districts (name) VALUES  ('Matara');
 
-INSERT INTO Districts (name) VALUES  ('Gampaha');
+--INSERT INTO Districts (name) VALUES  ('Ampara');
+--INSERT INTO Districts (name) VALUES  ('Anuradhapura');
+--INSERT INTO Districts (name) VALUES  ('Badulla');
+--INSERT INTO Districts (name) VALUES  ('Batticaloa');
+--INSERT INTO Districts (name) VALUES  ('Colombo');
 
-INSERT INTO Districts (name) VALUES  ('Jaffna');
-INSERT INTO Districts (name) VALUES  ('Kalutara');
-INSERT INTO Districts (name) VALUES  ('Kandy');
-INSERT INTO Districts (name) VALUES  ('Kegalle');
-INSERT INTO Districts (name) VALUES  ('Kilinochchi');
-INSERT INTO Districts (name) VALUES  ('Kurunegala');
-INSERT INTO Districts (name) VALUES  ('Mannar');
-INSERT INTO Districts (name) VALUES  ('Matale');
+--INSERT INTO Districts (name) VALUES  ('Gampaha');
 
-INSERT INTO Districts (name) VALUES  ('Monaragala');
-INSERT INTO Districts (name) VALUES  ('Mullaitivu');
-INSERT INTO Districts (name) VALUES  ('Nuwara Eliya	');
-INSERT INTO Districts (name) VALUES  ('Polonnaruwa');
-INSERT INTO Districts (name) VALUES  ('Puttalam');
-INSERT INTO Districts (name) VALUES  ('Ratnapura');
-INSERT INTO Districts (name) VALUES  ('Trincomalee');
-INSERT INTO Districts (name) VALUES  ('Vavuniya');
+--INSERT INTO Districts (name) VALUES  ('Jaffna');
+--INSERT INTO Districts (name) VALUES  ('Kalutara');
+--INSERT INTO Districts (name) VALUES  ('Kandy');
+--INSERT INTO Districts (name) VALUES  ('Kegalle');
+--INSERT INTO Districts (name) VALUES  ('Kilinochchi');
+--INSERT INTO Districts (name) VALUES  ('Kurunegala');
+--INSERT INTO Districts (name) VALUES  ('Mannar');
+--INSERT INTO Districts (name) VALUES  ('Matale');
+
+--INSERT INTO Districts (name) VALUES  ('Monaragala');
+--INSERT INTO Districts (name) VALUES  ('Mullaitivu');
+--INSERT INTO Districts (name) VALUES  ('Nuwara Eliya	');
+--INSERT INTO Districts (name) VALUES  ('Polonnaruwa');
+--INSERT INTO Districts (name) VALUES  ('Puttalam');
+--INSERT INTO Districts (name) VALUES  ('Ratnapura');
+--INSERT INTO Districts (name) VALUES  ('Trincomalee');
+--INSERT INTO Districts (name) VALUES  ('Vavuniya');
 
 
 INSERT INTO EducationZones (name) VALUES  ('Piliyandala');
